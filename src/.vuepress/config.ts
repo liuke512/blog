8a5affa1cc594984c143ee0ff35a2bd12170f305
刘科 <liuke@jizhibao.com.cn>
2024-12-06 11:17:11 +0800

    修改base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,10 +3,10 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/blog/",
 
   lang: "zh-CN",
-  title: "博客演示",
+  title: "koko博客",
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
